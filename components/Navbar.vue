7af2f5b33c317b9e8fac5d42ce54042facc6233c
whatAnswer <554528737@qq.com>
2019-11-29 10:05:32 +0800

    修改页面颜色

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,16 +3,16 @@
     <!-- active-text-color="#ffd04b" -->
     <el-menu default-active="2" @open="handleOpen" @close="handleClose" class="el-menu-vertical-demo" >
       <el-menu-item index="1111" @click="navTo('/home')">
-        <i class="el-icon-s-home"></i>
+        <i class="el-icon-s-home iconcolor"></i>
         <span slot="title">首页</span>
       </el-menu-item>
       <el-menu-item index="1112" @click="navTo('/goodsManage')">
-        <i class="el-icon-s-management"></i>
+        <i class="el-icon-s-management iconcolor"></i>
         <span slot="title">商品管理</span>
       </el-menu-item>
       <el-submenu index="1">
         <template slot="title">
-          <i class="el-icon-location"></i>
+          <i class="el-icon-location iconcolor"></i>
           <span>商品管理</span>
         </template>
         <el-menu-item-group>
@@ -28,15 +28,15 @@
         </el-submenu>
       </el-submenu>
       <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
+        <i class="el-icon-menu iconcolor"></i>
         <span slot="title">导航二</span>
       </el-menu-item>
       <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
+        <i class="el-icon-document iconcolor"></i>
         <span slot="title">导航三</span>
       </el-menu-item>
       <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
+        <i class="el-icon-setting iconcolor"></i>
         <span slot="title">导航四</span>
       </el-menu-item>
     </el-menu>
@@ -59,6 +59,9 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "@/assets/style/style.scss";
+.iconcolor{
+  color: $common-color;
+}
 .navbar {
   background-color: $sidebar-bg;
   /deep/ .el-menu {
